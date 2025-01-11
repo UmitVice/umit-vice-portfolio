33f5e33debb349d19d0682dd6c2a53725116e0c4
Umit Vice <umitvice@gmail.com>
2025-01-11 20:12:46 +0300

    Create Header

---
+++ src/components/Header.tsx/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-primary text-white fixed top-0 w-full z-10 shadow-md">
+      <nav className="container mx-auto flex justify-between items-center p-4">
+        <div className="text-xl font-bold">My Portfolio</div>
+        <ul className="hidden md:flex space-x-6">
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/projects">Projects</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
+        <div className="md:hidden">
+          <button className="p-2">Menu</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
